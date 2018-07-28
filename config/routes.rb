5b5f5472b7e48221481b3edf37d94103e4642e3a
Matsuo Kyoushirou <makkyoru0129@gmail.com>
2018-07-28 19:54:43 +0900

    ログインボタンの名称を「新規作成」から「ログイン」に変更 左上にタイトルつけて、真ん中の文字を小さくしていろいろ微調整。 width100%の記述は少なくとも俺のchromeだとなくてもよかったので消した。 あとheaderタグは完全に不要だったので消した。 トップページに背景画像をつけた。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'main/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :login
   get 'login/verymuch', to: 'login#verymuch'
-  root 'application#index'
+  root 'login#index'
 end
