921de556726241cda29ae102a22697a5605526a5
Matsuo Kyoushirou <makkyoru0129@gmail.com>
2018-08-08 13:27:59 +0900

    rubocopに従ってコードスタイルを修正。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,34 @@
+# Userモデル<br>
+# どういう扱いにするかは今後考えるけど、いまは名前とパスワードだけ持ってる。<br>
+# 認証用のユーザー一覧のような立ち位置にしてしまった方がいいかも。<br>
+# てかモデルにフィールド書かないからUserテーブルの定義わかんねえな。
 class User < ApplicationRecord
   extend MsgConst
 
-  NAME = "名前"
-  validates :name,
-    presence: {message: MsgConst.read(1, NAME)},
-    length: {maximum: 20, message: MsgConst.read(2, NAME, 20)},
-    uniqueness: {case_sensitive: false, message: MsgConst.read(3),
-                 unless: proc { [:signin].include?(validation_context) }}
+  NAME = '名前'.freeze
+  validates(
+    :name,
+    presence: { message: MsgConst.read(1, NAME) },
+    length: { maximum: 20, message: MsgConst.read(2, NAME, 20) },
+    uniqueness: { case_sensitive: false, message: MsgConst.read(3),
+                  unless: proc { [:signin].include?(validation_context) } }
+  )
 
-  PASSWORD = "パスワード"
-  validates :password,
-    presence: {message: MsgConst.read(1, PASSWORD)},
-    length: {maximum: 30, message: MsgConst.read(2, PASSWORD, 30)}
+  PASSWORD = 'パスワード'.freeze
+  validates(
+    :password,
+    presence: { message: MsgConst.read(1, PASSWORD) },
+    length: { maximum: 30, message: MsgConst.read(2, PASSWORD, 30) }
+  )
 
   validate :already_registered, on: :signin
 
   # name,passwordに一致するレコードがDBにないならNG
   def already_registered
     # すでにエラーがあるときは検証しない
-    return "" if errors.present?
+    return '' if errors.present?
 
     user = User.find_by(name: name, password: password)
-    if user.blank?
-      errors.add(:name, MsgConst.read(4))
-    end
+    user.blank? && errors.add(:name, MsgConst.read(4))
   end
 end
